d062c9dedd13f85fe3043445fe79759cf0f55fa8
faustinopsy <rodrigohipnose@gmail.com>
2025-07-15 19:07:24 -0300

    ajuste para carregamento pela tag defer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,14 @@
-// assets/js/main.js
 import { App } from './components/App.js';
+import { SettingsService } from './services/SettingsService.js';
 import { ProfileService } from './services/ProfileService.js';
 import { SeenService } from './services/SeenService.js';
-import { SettingsService } from './services/SettingsService.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const appElement = document.getElementById('app');
-    
-    const seenService = new SeenService();
-    const settingsService = new SettingsService();
-    
-    const profileService = new ProfileService(seenService);
-    
-    const myApp = new App(appElement, profileService, seenService, settingsService);
-    myApp.init();
-});
+const appElement = document.getElementById('app');
+
+const seenService = new SeenService();
+const settingsService = new SettingsService();
+
+const profileService = new ProfileService(seenService);
+
+const myApp = new App(appElement, profileService, seenService, settingsService);
+myApp.init();
